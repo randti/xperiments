e7dcc78ad5e69eab2857c5f4a45657f375169c74
Ewgenij Denisow <jenyaremake@gmail.com>
2018-12-30 12:59:37 +0300

    Fignya

--- xp1.py
+++ xp1.py
@@ -1,6 +1,6 @@
+a=[]
 for i in range(10):
     for j in range(10):
         for p in range(10):
-            print(i,j,p)
-            print(1)
-            print(2)
+            a.append(tuple(((i,j,p))))
+print(*a)
