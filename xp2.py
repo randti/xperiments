65c3aaabc11b4385a21b3971e574c47cb45dc847
Ewgenij Denisow <jenyaremake@gmail.com>
2019-01-01 13:57:24 +0300

    Peres

--- xp2.py
+++ xp2.py
@@ -1,6 +1,13 @@
-def gcd(a, b):
-    if b == 0:
-        return a
-    return gcd(b, a % b)
-a,b=map(int,input().split())
-print((a*b)//gcd(a,b))
+a,b,c=(map(int,input().split()))
+p=[]
+for i in range(1,a+1):
+    p=i+1
+    d=p+1
+    if b>p and c>d:
+        j=1
+    else:
+        print(i+p+d)
+        break
+
+
+
